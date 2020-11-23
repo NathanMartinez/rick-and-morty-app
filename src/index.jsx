fc83baa462cc2cce36a44b55e16a78cf830ce675
Nate Martinez <natemartinez@Nates-iMac.local>
2020-11-22 19:32:26 -0700

    Updated Scss settings to compress css

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/css/index.css";
+import "./styles/css/index.min.css";
 import App from "./components/App";
 import TestCharacterProvider from "./context/TestCharacterContext";
 
